17ff4a22200bf3cda206f7bd7ec83a0eec93091c
Adarsh Raj <ahazadarsh0014@gmail.com>
2023-05-09 08:13:27 +0530

    first commit -adarsh 3

--- src/Api/AuthRequest.js
+++ src/Api/AuthRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-//const API=axios.create({baseURL:"https://server.possindia.in"})
-const API=axios.create({baseURL:"http://localhost:5005"})
+const API=axios.create({baseURL:"https://server.possindia.in"})
+//const API=axios.create({baseURL:"http://localhost:5005"})
 
 export const signUp=(data)=>API.post('/auth/register',data)
 export const login=(data)=>API.post('/auth/login',data)
